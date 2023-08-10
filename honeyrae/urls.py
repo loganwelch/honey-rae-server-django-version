bad5d6117b5dc8e255f63aa6caa84b9636c10852
Logan Welch <loganewelch@gmail.com>
2023-08-10 15:02:50 -0500

    added functionality for full_name feature to show tickets having full_name property

---
+++ honeyrae/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+from repairsapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from repairsapi.views import CustomerView, EmployeeView, ServiceTicketView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
+router.register(r'employees', EmployeeView, 'employee')
+router.register(r'tickets', ServiceTicketView, 'ticket')
+
+urlpatterns = [
+    path('register', register_user),
+    path('login', login_user),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
---
+++ repairsapi/views/service_ticket_view.py
@@ -0,0 +1,59 @@
+"""View module for handling requests for customer data"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from repairsapi.models import ServiceTicket, Employee, Customer
+
+
+class ServiceTicketView(ViewSet):
+    """Honey Rae API customers view"""
+
+    def list(self, request):
+        """Handle GET requests to get all customers
+
+        Returns:
+            Response -- JSON serialized list of customers
+        """
+
+        if request.auth.user.is_staff:
+            service_tickets = ServiceTicket.objects.all()
+
+        else:
+            service_tickets = ServiceTicket.objects.filter(customer__user=request.auth.user)
+        serialized = ServiceTicketSerializer(service_tickets, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single customer
+
+        Returns:
+            Response -- JSON serialized customer record
+        """
+
+        serviceTicket = ServiceTicket.objects.get(pk=pk)
+        serialized = ServiceTicketSerializer(serviceTicket, context={'request': request})
+        return Response(serialized.data, status=status.HTTP_200_OK)
+
+
+class TicketEmployeeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employee
+        fields = ('id', 'full_name', 'specialty')
+
+class TicketCustomerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Customer
+        fields = ('id', 'full_name', 'address')
+
+class ServiceTicketSerializer(serializers.ModelSerializer):
+    """JSON serializer for customers"""
+    employee = TicketEmployeeSerializer(many=False)
+    customer = TicketCustomerSerializer(many=False)
+
+    class Meta:
+        model = ServiceTicket
+        fields = ('id', 'customer', 'employee', 'description', 'emergency', 'date_completed')
+        depth = 1
